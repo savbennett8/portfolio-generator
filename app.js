0ccfe0f6d7ea3eb04ced0e3f0d95fc4127401de2
savbennett8 <email@example.com>
2021-03-18 19:38:34 -0600

    inquirer install & require

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
-const fs = require('fs');
-//imports data from the required relative location
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-//array which holds the user command line arguments
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-//extract & store those arguments using assignment destructuring
-const [name, github] = profileDataArgs;
-
-
-//alternatively written using the array index:
-//const name = profileDataArgs[0];
-//const github = profileDataArgs[1];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// ----------------------need this later but not now -----------------//
 
-    console.log('Portfolio complete! Check out the index.html to see the output!');
-});
+// //requires file system from node
+// const fs = require('fs');
+// //imports data from the required relative location
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// // ---------------- remove bc using inquirer to capture data now ------------- //
+// // //array which holds the user command line arguments
+// // const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// // //extract & store those arguments using assignment destructuring
+// // const [name, github] = profileDataArgs;
+
+
+// //alternatively written using the array index:
+// //const name = profileDataArgs[0];
+// //const github = profileDataArgs[1];
+
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out the index.html to see the output!');
+// });
